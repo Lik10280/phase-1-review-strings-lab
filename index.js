3d1f06a76fefc678bb899759b15ad2bbdb30196f
+ <+>
2022-11-23 15:09:30 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Kamau";
+const welcomeMessage ="Welcome to Flatbook, ${Kamau}!";
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, ${KAMAU}!";
+const shortGreeting = 'Welcome, ${K}!';
